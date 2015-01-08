82941f0dbcbc8e7247008cc82d34d9bc98cc9cf8
ronald8192 <u23805@gmail.com>
2015-01-08 16:17:26 +0800

    use Collections.synchronizedList

--- src/com/ronald8192/playingwiththreads/ThreadWithQueue.java
+++ src/com/ronald8192/playingwiththreads/ThreadWithQueue.java
@@ -1,40 +1,43 @@
 package com.ronald8192.playingwiththreads;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ThreadWithQueue implements Runnable {
 	private static int totalPause = 32; // how many times to pause
 	private static int pauseTime = 1000; // ms, how long does the threads will
-										// pause each time
+											// pause each time
 	private static ConcurrentLinkedQueue<Integer> clq = new ConcurrentLinkedQueue<Integer>();
 	private static long begin = -1; // program begin time
 	private static int threadRunning = 0; // count how many threads are running
 	private Thread t;
 	// the result from queue
-	public static List<Integer> theOutput = new ArrayList<Integer>(); 
-	
+	private static List<Integer> theOutput = new ArrayList<Integer>();
+	private static List<Integer> synclist = Collections.synchronizedList(theOutput);
 	/**
-	 * Constructor.
-	 * Here just for initialize the static variable/object.
+	 * Initialize queue
+	 */
+	static {
+		for (int i = 0; i < totalPause; i++) {
+			clq.offer(i);
+		}
+		System.out.println("clq.size(): " + clq.size());
+	}
+
+	/**
+	 * Constructor
 	 */
 	public ThreadWithQueue() {
-		// init queue
-		if (clq.size() == 0) {
-			for (int i = 0; i < totalPause; i++) {
-				clq.offer(i);
-			}
-			System.out.println("clq.size(): " + clq.size());
-		}
 	}
 
 	@Override
 	public void run() {
 		if (begin == -1) begin = System.currentTimeMillis();
 		while (clq.size() > 0) {
-			//ConcurrentLinkedQueue is thread-safe
-			//int sthToProcess = pollResourcesFromQueue();
+			// ConcurrentLinkedQueue is thread-safe
+			// int sthToProcess = pollResourcesFromQueue();
 			int sthToProcess = clq.poll();
 			try {
 				// do the thing
@@ -42,15 +45,18 @@ public class ThreadWithQueue implements Runnable {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			storeResult(sthToProcess);
-
+			synclist.add(sthToProcess);
+			//use synchronized method if List is not thread-safe
+			//storeResult(sthToProcess);
 		}
 
 		// job done, thread will be terminate
 		System.out.println(threadCountDecrement() + " threads is still running");
 	}
+
 	/**
-	 * Start a new thread if queue have any element and thread is not started before.
+	 * Start a new thread if queue have any element and thread is not started
+	 * before.
 	 */
 	public void start() {
 		if (t == null && clq.size() > 0) {
@@ -60,40 +66,46 @@ public class ThreadWithQueue implements Runnable {
 			System.out.println("ThreadWithQueue.start()");
 		}
 	}
+
 	/**
-	 * Use this if the queue is not thread-safe.
-	 * Get and remove next element from queue. 
+	 * Use this if the queue is not thread-safe. Get and remove next element
+	 * from queue.
+	 * 
 	 * @return next queue element
 	 */
 	@SuppressWarnings("unused")
 	synchronized private static int pollResourcesFromQueue() {
 		return clq.poll();
 	}
-	
+
 	/**
-	 * Store the result.
+	 * Store the result to non-thread-safe List.
+	 * 
 	 * @param result
 	 */
+	@SuppressWarnings("unused")
 	synchronized private static void storeResult(int result) {
 		theOutput.add(result);
 	}
-	
+
 	/**
-	 * Decrement thread count.
-	 * Print time token and all the result when all threads terminated.
+	 * Decrement thread count. Print time token and all the result when all
+	 * threads terminated.
+	 * 
 	 * @return number of threads are running.
 	 */
 	synchronized private static int threadCountDecrement() {
 		if (--threadRunning == 0) {
 			System.out.println("Time token:" + (System.currentTimeMillis() - begin));
-			System.out.println(ThreadWithQueue.theOutput.toString());
+			System.out.println(ThreadWithQueue.synclist.toString());
 		}
 		return threadRunning;
 
 	}
-	
+
 	/**
 	 * Increment thread count.
+	 * 
 	 * @return number of threads are running.
 	 */
 	synchronized private static int threadCountIncrement() {
